e6672ff507d10afbcbcb3e66fe98fdf8898e75f0
Ivan Cherkas <ivan.cherkas@ICHERKAS.intexsoft.by>
2023-02-20 12:42:12 +0300

    init cv burger-menu

---
+++ src/app/core/layout/components/navigation-panel-burger/navigation-panel-burger.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cv-navigation-panel-burger',
+  templateUrl: './navigation-panel-burger.component.html',
+  styleUrls: ['./navigation-panel-burger.component.scss']
+})
+export class NavigationPanelBurgerComponent {
+
+}
