bac35c242b57687ed0af374e73d293b1fa9e1a1d
Ivan Cherkas <ivan.cherkas@ICHERKAS.intexsoft.by>
2023-02-20 12:10:07 +0300

    fixed positions of header/fixed links/add font zekton

--- src/app/core/layout/components/footer/footer.component.ts
+++ src/app/core/layout/components/footer/footer.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { socialMediaLinks } from '../../../../../assets/constants/social-media-linke';
+
 @Component({
   selector: 'cv-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-
+  public socialLinks = socialMediaLinks;
 }
--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -20,9 +20,7 @@ export class LayoutComponent {
   basicTable: any;
 
   constructor(private http: HttpClient, private gitHubService: GitHubService) {
-    this.gitHubService.getUserRepos().subscribe((el) => {
-      console.log(el);
-    });
+    this.gitHubService.getUserRepos().subscribe((el) => {});
   }
 
   getItems(): Observable<Item[]> {
