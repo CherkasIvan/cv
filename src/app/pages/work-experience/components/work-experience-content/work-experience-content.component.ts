bcd2e431301d1a2a8025205d7a526b07ab36f0a5
Ivan Cherkas <ivan.cherkas@ICHERKAS.intexsoft.by>
2023-02-23 17:04:45 +0300

    add work exp conent

--- src/app/pages/work-experience/components/work-experience-content/work-experience-content.component.ts
+++ src/app/pages/work-experience/components/work-experience-content/work-experience-content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { workExperience } from 'src/assets/constants/work-experience';
 
 @Component({
   selector: 'cv-work-experience-content',
   templateUrl: './work-experience-content.component.html',
-  styleUrls: ['./work-experience-content.component.scss']
+  styleUrls: ['./work-experience-content.component.scss'],
 })
 export class WorkExperienceContentComponent {
-
+  public workPlace = workExperience;
 }
--- src/assets/constants/work-experience.ts
+++ src/assets/constants/work-experience.ts
@@ -1,28 +1,36 @@
 export const workExperience: any[] = [
   {
-    place: 'assets/img/social-media/linkedin.png',
-    from: 'linkedin',
-    to: '',
+    logo: '',
+    company: 'TeachMeSkills',
+    from: '01-10-2020',
+    to: '01-08-2021',
+    place: 'Минск, Республика Беларусь',
     link: 'https://www.linkedin.com/in/ivan-cherkas-723b411a2',
   },
   {
-    place: 'assets/img/social-media/github.png',
-    from: 'linkedin',
-    to: '',
+    logo: '',
+    company: 'Exadel',
+    from: '01-01-2021',
+    to: '01-04-2021',
+    place: 'Минск, Республика Беларусь',
     link: 'https://github.com/CherkasIvan',
   },
 
   {
-    place: 'assets/img/social-media/telegram.png',
-    from: 'linkedin',
-    to: '',
+    logo: '',
+    company: 'FreshLimeSoft',
+    from: '01-08-2021',
+    to: '22-12-2022',
+    place: 'Минск, Республика Беларусь',
     link: 'https://t.me/IvanCherkas',
   },
 
   {
-    place: 'assets/img/social-media/facebook.png',
-    from: 'linkedin',
-    to: '',
+    logo: '',
+    company: 'IntexSoft',
+    from: '01-08-2021',
+    to: 'present time',
+    place: 'Минск, Республика Беларусь',
     link: 'https://www.facebook.com/ivan.cherkas',
   },
 ];
