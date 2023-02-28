0ca6cf194d248499cb3b3bdf44c21d8618054245
Ivan Cherkas <ivan.cherkas@ICHERKAS.intexsoft.by>
2023-02-28 12:01:00 +0300

    add education and fix some styles

--- src/app/pages/education/components/education-content/education-content.component.ts
+++ src/app/pages/education/components/education-content/education-content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { education } from 'src/assets/constants/education';
 
 @Component({
   selector: 'cv-education-content',
   templateUrl: './education-content.component.html',
-  styleUrls: ['./education-content.component.scss']
+  styleUrls: ['./education-content.component.scss'],
 })
 export class EducationContentComponent {
-
+  public educationList = education;
 }
---
+++ src/assets/constants/education.ts
@@ -0,0 +1,35 @@
+export const education: any[] = [
+  {
+    educationTitle: 'Школа',
+    educationPlaceName: [
+      {
+        name: 'Средняя школа №132',
+        link: 'http://sch132.minsk.edu.by/',
+        address: 'г.Минск, ул.П.Глебки, д.86',
+        phone: '+375 17 337-46-43 ',
+      },
+    ],
+  },
+  {
+    educationTitle: 'Университет',
+    educationPlaceName: [
+      {
+        name: 'Минский инновационный университет',
+        link: 'http://miu.by/',
+        address: 'г.Минск, ул.Лазо, д.12',
+        phone: '+375 17 351-26-27',
+      },
+    ],
+  },
+  {
+    educationTitle: 'Курсы',
+    educationPlaceName: [
+      {
+        name: 'TeachMeSkills',
+        link: 'https://teachmeskills.by/',
+        address: 'г.Минск, ул.Тимирязева, д.67, оф.1101',
+        phone: '+375 29 105-59-59',
+      },
+    ],
+  },
+];
