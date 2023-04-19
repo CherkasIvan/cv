bede0548cdaeb6838dece6153f24cc24a4cbaa94
Ivan Cherkas <ivan.cherkas@ICHERKAS.intexsoft.by>
2023-04-19 12:34:03 +0300

    init func to month counting

--- src/app/pages/work-experience/components/work-experience-content/work-experience-content.component.ts
+++ src/app/pages/work-experience/components/work-experience-content/work-experience-content.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
 import { workExperience } from 'src/assets/constants/work-experience';
+import * as moment from 'moment';
 
 @Component({
   selector: 'cv-work-experience-content',
@@ -9,4 +9,53 @@ import { workExperience } from 'src/assets/constants/work-experience';
 })
 export class WorkExperienceContentComponent {
   public workPlace = workExperience;
+  public workExp: any[] = [];
+
+  public totalWorkTime(from: string, to: string) {
+    const dateFromFormat = moment(from, 'DD-MM-YYYY');
+    const dateToFormat = moment(to, 'DD-MM-YYYY');
+
+    if (to === 'present time' || to === 'по настоящее время') {
+      const today = moment(new Date()).format('DD-MM-YYYY');
+      const now = moment(today, 'DD-MM-YYYY');
+      const diff = moment(now).diff(moment(dateFromFormat), 'days');
+      return this.workTimeFormat(diff);
+    } else {
+      const diff = moment(dateToFormat).diff(moment(dateFromFormat), 'days');
+      return this.workTimeFormat(diff);
+    }
+  }
+
+  public workTimeFormat(days: number) {
+    const months = Math.ceil(days / 30);
+    let yearsActual;
+    let actualWorkTime;
+    let monthActual = months;
+
+    if (months >= 12) {
+      yearsActual = Math.round(months / 12);
+      monthActual = months - yearsActual * 12;
+      actualWorkTime =
+        (yearsActual === 1
+          ? `${yearsActual} год`
+          : `${yearsActual} года` && monthActual < 5) +
+        ' ' +
+        (monthActual < 5 && monthActual > 1
+          ? `${monthActual} месяца`
+          : `${monthActual} месяцев`);
+    } else {
+      actualWorkTime =
+        monthActual < 5 && monthActual > 1
+          ? `${monthActual} месяца`
+          : `${monthActual} месяцев`;
+    }
+    return actualWorkTime;
+  }
+
+  ngOnInit() {
+    this.workPlace.forEach((work) =>
+      this.workExp.push(this.totalWorkTime(work.from, work.to))
+    );
+    console.log(this.workExp);
+  }
 }
