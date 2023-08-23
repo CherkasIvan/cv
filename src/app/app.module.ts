d3b5d989f06843c95308b016bd903e7330cdc5fa
CherkasIvan <distor6on@yandex.ru>
2023-08-23 12:57:08 +0300

    updated cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
@@ -13,10 +13,10 @@ import { AppComponent } from './app.component';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import {
-	provideAnalytics,
-	getAnalytics,
-	ScreenTrackingService,
-	UserTrackingService,
+  provideAnalytics,
+  getAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
 } from '@angular/fire/analytics';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideDatabase, getDatabase } from '@angular/fire/database';
@@ -30,37 +30,37 @@ import { EntityDataModule } from '@ngrx/data';
 import { entityConfig } from './entity-metadata';
 
 export function HttpLoaderFactory(http: HttpClient) {
-	return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 @NgModule({
-	declarations: [AppComponent],
-	imports: [
-		BrowserModule.withServerTransition({ appId: 'serverApp' }),
-		TranslateModule.forRoot({
-			loader: {
-				provide: TranslateLoader,
-				useFactory: HttpLoaderFactory,
-				deps: [HttpClient],
-			},
-			defaultLanguage: 'ru',
-		}),
-		AppRoutingModule,
-		BrowserAnimationsModule,
-		HttpClientModule,
-		CoreModule,
-		provideFirebaseApp(() => app),
-		provideAnalytics(() => analytics),
-		provideAuth(() => getAuth()),
-		provideDatabase(() => getDatabase()),
-		provideFirestore(() => getFirestore()),
-		provideFunctions(() => getFunctions()),
-		StoreModule.forRoot({}, {}),
-		EffectsModule.forRoot([]),
-		StoreRouterConnectingModule.forRoot(),
-		EntityDataModule.forRoot(entityConfig),
-	],
-	providers: [ScreenTrackingService, UserTrackingService],
-	bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+      defaultLanguage: 'ru',
+    }),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CoreModule,
+    provideFirebaseApp(() => app),
+    provideAnalytics(() => analytics),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot(),
+    EntityDataModule.forRoot(entityConfig),
+  ],
+  providers: [ScreenTrackingService, UserTrackingService],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
