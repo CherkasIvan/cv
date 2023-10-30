a43d1f41626c3fbb68a31d3d1aad30678e8afb49
Ivan Cherkas <cherkas.ivan13@gmail.com>
2023-10-30 04:53:11 +0300

    set logout dialog

---
+++ src/app/core/services/logout-dialog/logout-dialog.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+import { LogoutDialogComponent } from '@app/core/components/logout-dialog/logout-dialog.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogoutDialogService {
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(LogoutDialogComponent, {
+      width: '600px'
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      return result;
+    });
+  }
+
+  closeDialog(): void {
+    this.dialog.closeAll();
+  }
+}
