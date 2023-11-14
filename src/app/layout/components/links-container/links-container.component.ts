20079c9e0ff1f74a12624a82910eebdbcad8ac2e
I.Cherkas <I.Cherkas@tanto-s.ru>
2023-11-14 09:30:19 +0300

    added few icon in frontend tech and added mattooltip into links

--- src/app/layout/components/links-container/links-container.component.ts
+++ src/app/layout/components/links-container/links-container.component.ts
@@ -1,16 +1,16 @@
-import { Observable } from 'rxjs';
-
+import { CdkScrollable } from '@angular/cdk/scrolling';
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { DomSanitizer } from '@angular/platform-browser';
-
 import { ISocialMedia } from '@shared/models/social-media.interface';
 import { FirebaseService } from '@shared/services/firebase/firebase.service';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'cv-links-container',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, MatTooltipModule, CdkScrollable],
     templateUrl: './links-container.component.html',
     styleUrls: ['./links-container.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
@@ -18,8 +18,7 @@ import { FirebaseService } from '@shared/services/firebase/firebase.service';
 export class LinksContainerComponent {
     @Input() public modeTheme!: boolean;
     @Input() public pwaView = false;
-    public socialMediaLinks$: Observable<ISocialMedia[]> =
-        this._firebaseService.getSocialMediaLinks();
+    public socialMediaLinks$: Observable<ISocialMedia[]> = this._firebaseService.getSocialMediaLinks();
 
     constructor(
         private readonly _firebaseService: FirebaseService,
